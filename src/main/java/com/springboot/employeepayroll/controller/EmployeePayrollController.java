b0db3c2c7b2fbdc1fe849809206aa6281472ce2b
Prajwal Sanjay Thorat <prajwalthorat9007@gmail.com>
2026-03-23 00:15:35 +0530

    [Prajwal]:UC1:Create Employee Payroll Spring Project to cater REST request from UI and test using CURL.

---
+++ src/main/java/com/springboot/employeepayroll/controller/EmployeePayrollController.java
@@ -0,0 +1,13 @@
+package com.springboot.employeepayroll.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmployeePayrollController {
+
+    @GetMapping("/")
+    public String sayHello() {
+        return "Employee Payroll App is running";
+    }
+}
